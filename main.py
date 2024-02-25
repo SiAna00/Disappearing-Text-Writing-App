a6a79183048222790745c0e56548d1f8fc40493e
anasi <asimicev@gmail.com>
2024-02-26 00:02:52 +0100

    change text.destroy() to text.delete()

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from tkinter import *
 import threading
 
-def create_text_field():
-    global text
-    text = Text(root, height=30, width=60)
-    text.grid(column=0, row=3, columnspan=3)
-
-    text.bind("<KeyRelease>", key_released)
-
 
 def check_if_pressed():
     if pressed == "no":
-        text.destroy()
+        text.delete("1.0", END)
 
     elif pressed == "yes":
         return
@@ -40,8 +33,10 @@ def key_released(e):
 root = Tk()
 root.config(padx=40, pady=40)
 
-start_button = Button(root, text="Start", command=create_text_field)
-start_button.grid(column=1, row=1, padx=10, pady=10)
+text = Text(root, height=30, width=60)
+text.grid(column=0, row=3, columnspan=3)
+
+text.bind("<KeyRelease>", key_released)
 
 
 root.mainloop()
