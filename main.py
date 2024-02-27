c427e089112c3c5104413a2aef829c5bc281e3e0
anasi <asimicev@gmail.com>
2024-02-27 14:35:43 +0100

    change UI, add notification_label

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from tkinter import *
 import threading
+import time
+
+
+def clear():
+    global notification_label
+
+    notification_label.config(text="")
 
 
 def check_if_pressed():
+    global notification_label
+
     if pressed == "no":
         text.delete("1.0", END)
+        notification_label.config(text="Hurry up...")
 
     elif pressed == "yes":
         return
@@ -31,10 +41,20 @@ def key_released(e):
 
 
 root = Tk()
-root.config(padx=40, pady=40)
+root.config(padx=60, pady=60, background="floral white")
+root.title("The Most Dangerous Writing App")
+           
+intro_label = Label(root, text="Welcome to the Most Dangerous Writing App", font="Courier 24 bold", background="floral white")
+intro_label.grid(column=1, row=0)
 
-text = Text(root, height=30, width=60)
-text.grid(column=0, row=3, columnspan=3)
+notification_label = Label(root, text="", font="Courier 14 bold", background="floral white")
+notification_label.grid(column=1, row=2, pady=20)
+
+start_button = Button(root, text="Start", font="Courier 14 bold", background="gray12", foreground="whitesmoke", command=clear)
+start_button.grid(column=1, row=3, pady=20, ipadx=15, ipady=5)
+
+text = Text(root, height=30, width=60, font="Courier 14", background="antique white")
+text.grid(column=0, row=4, columnspan=3)
 
 text.bind("<KeyRelease>", key_released)
 
